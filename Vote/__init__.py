c85efa6761419242c3ee287d7f620812d7832b3e
SUNken2021 <82763863+SUNken2021@users.noreply.github.com>
2022-10-01 00:36:54 +0800

    Delete Vote directory

--- Vote/__init__.py
+++
@@ -1,5 +0,0 @@
-from Vote.Vote import Vote_quadratic
-from Vote.Vote_sequential import Vote_condition
-from Vote.Vote_sequential import Vote_Sequential
-
-__all__=['Vote_quadratic', 'Vote_Sequential', 'Vote_condition']
